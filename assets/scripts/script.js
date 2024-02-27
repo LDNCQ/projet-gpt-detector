f1c467a442981c242c316cc28ba1412c09dd7de7
LDNCQ <willylin11@gmail.com>
2024-02-27 16:54:24 +0100

    ctrl v bugged

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,8 @@
-// let tr = document.getElementById("therock")
+let tr = document.getElementById("therock")
 // let tr2 = document.getElementById("therock2")
 // let tr3 = document.getElementById("therock3")
+let boom = document.getElementById("boom-vine-sf");
+let amogus = document.getElementById("amogus-sf");
 
 document.addEventListener("visibilitychange", function() {
     if (document.visibilityState === 'hidden') {
@@ -8,32 +10,32 @@ document.addEventListener("visibilitychange", function() {
     }
   });
   
-  document.addEventListener("keydown", function(e) {
-      if ((e.ctrlKey || e.metaKey) && (e.key === "c" || e.key === "C")) {
-          console.log("Ctrl+C pressed");
-          // tr.style.display = "block";
-          // setTimeout(() => {
-          //     tr.style.display = "none";
-          // }, 200);
-  
-      } else if ((e.ctrlKey || e.metaKey) && (e.key === "v" || e.key === "V")) {
-          console.log("Ctrl+V pressed");
-          navigator.clipboard.writeText("You shall not paste");
-  
-          // tr3.style.display = "block";
-          // setTimeout(() => {
-          //     tr3.style.display = "none";
-          // }, 200);
-  
-      } else if (e.key === "F12") {
-          console.log("F12 pressed");
-          // tr2.style.display = "block";
-          // setTimeout(() => {
-          //     tr2.style.display = "none";
-          // }, 200);
-          e.preventDefault();
-      }
-  });     
+document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey) {
+        navigator.clipboard.writeText("You shall not paste");
+        console.log("Ctrl pressed");
+    } else if ((e.ctrlKey || e.metaKey) && (e.key === "v" || e.key === "V")) {
+        console.log("Ctrl+V pressed");
+        boom.play();
+
+        tr.style.display = "block";
+        setTimeout(() => {
+            tr.style.display = "none";
+        }, 200);
+
+    } else if (e.key === "F12") {
+        console.log("F12 pressed");
+        // tr2.style.display = "block";
+        // setTimeout(() => {
+        //     tr2.style.display = "none";
+        // }, 200);
+        e.preventDefault();
+    } else if ((e.ctrlKey || e.metaKey) && e.shiftKey && (e.key === "i" || e.key === "I" || e.key === "j" || e.key === "J")) {
+        console.log("Ctrl+Shift+I pressed");
+        e.preventDefault();
+    } 
+    console.log(e.key);
+});     
   
   
   document.addEventListener("contextmenu", function(e) {
