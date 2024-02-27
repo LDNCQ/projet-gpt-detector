27f7ba9a2c6d8e391c8fdec3d37d0c76e9b7f6a3
LDNCQ <willylin11@gmail.com>
2024-02-27 16:17:08 +0100

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,52 @@
+// let tr = document.getElementById("therock")
+// let tr2 = document.getElementById("therock2")
+// let tr3 = document.getElementById("therock3")
+
 document.addEventListener("visibilitychange", function() {
-  if (document.visibilityState === 'hidden') {
-    console.log("Changement de fenetre/onglet detecté");
-  }
-});
-
-document.addEventListener("keydown", function(e) {
-    if (e.ctrlKey && (e.key === "c" || e.key === "C")) {
-        console.log("Ctrl+C pressed");
+    if (document.visibilityState === 'hidden') {
+      console.log("Changement de fenetre/onglet detecté");
     }
-    if (e.ctrlKey && (e.key === "v" || e.key === "V")) {
-        console.log("Ctrl+V pressed");
-    }
-    if (e.key === "F12") {
-        console.log("F12 pressed");
-        e.preventDefault();
-    }
-});     
-
-document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
-    console.log("Right click disabled");
-});
-
+  });
+  
+  document.addEventListener("keydown", function(e) {
+      if ((e.ctrlKey || e.metaKey) && (e.key === "c" || e.key === "C")) {
+          console.log("Ctrl+C pressed");
+          // tr.style.display = "block";
+          // setTimeout(() => {
+          //     tr.style.display = "none";
+          // }, 200);
+  
+      } else if ((e.ctrlKey || e.metaKey) && (e.key === "v" || e.key === "V")) {
+          console.log("Ctrl+V pressed");
+          navigator.clipboard.writeText("You shall not paste");
+  
+          // tr3.style.display = "block";
+          // setTimeout(() => {
+          //     tr3.style.display = "none";
+          // }, 200);
+  
+      } else if (e.key === "F12") {
+          console.log("F12 pressed");
+          // tr2.style.display = "block";
+          // setTimeout(() => {
+          //     tr2.style.display = "none";
+          // }, 200);
+          e.preventDefault();
+      }
+  });     
+  
+  
+  document.addEventListener("contextmenu", function(e) {
+      e.preventDefault();
+      console.log("Right click disabled");
+  });
+  
+  //     const inputs = []
+  // document.addEventListener("keydown", function(e) {
+  
+  //     inputs.push(e.key);
+  //     console.log(inputs);
+  // }
+  // )
+  
+  
