ee5125ec1237c02e10c18255725ed3eaee2114f8
LDNCQ <willylin11@gmail.com>
2024-02-28 11:08:37 +0100

    update

---
+++ assets/scripts/supabase.js
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://tknqsoufdrwetvloclqo.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrbnFzb3VmZHJ3ZXR2bG9jbHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkwNDAzNjYsImV4cCI6MjAyNDYxNjM2Nn0.CP40RI-Co5YYTgEGXOjaG-leRl_Qv1OH9TMe8d1NqIA"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+async function getTableData() {
+    const { data, error } = await supabase
+    .from('table-test')
+    .select()
+    return {data, error}
+    console.log(data, error)
+}
